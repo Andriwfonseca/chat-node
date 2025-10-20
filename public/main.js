38384bfe09f514de1fdac3c39b0fd65911a20c5d
Andriw Fonseca <andriw.fonseca@totvs.com.br>
2025-10-20 16:07:48 -0300

    feat: entering the chat

--- public/main.js
+++ public/main.js
@@ -1,1 +1,48 @@
 const socket = io();
+let username = '';
+let userList = [];
+
+const loginPage = document.getElementById('login-page');
+const chatPage = document.getElementById('chat-page');
+
+const loginInput = document.getElementById('username');
+const messageInput = document.getElementById('message');
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+loginInput.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+   const name = loginInput.value.trim();
+
+   if (name != "") {
+    username = name;
+    document.title = `Chat - (${username})`;
+
+    socket.emit('join-room-request', username);
+
+  }
+}
+});
+
+function renderUserList() {
+  const userListElement = document.querySelector('.user-list');
+  userListElement.innerHTML = '';
+  
+  userList.forEach(user => {
+    userListElement.innerHTML += `<li>${user}</li>`;
+  });
+}
+
+socket.on('join-room-response', (data) => {
+  if (data.success) {
+    userList = data.users;
+    console.log('Usuários conectados: ', userList);
+
+    loginPage.style.display = 'none';
+    chatPage.style.display = 'flex';
+    messageInput.focus();
+
+    renderUserList();
+  }
+});
--- server.js
+++ server.js
@@ -13,8 +13,22 @@ server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
+const connectedUsers = [];
+
 io.on('connection', (socket) => {
   console.log('Um usuário se conectou: ', socket.id);
 
-  
+
+  socket.on('join-room-request', (username) => {
+    socket.username = username;
+
+    connectedUsers.push(username);
+
+    console.log('Usuários conectados: ', connectedUsers);
+
+    socket.emit('join-room-response', {
+      success: true,
+      users: connectedUsers
+    });
+  });
 });
