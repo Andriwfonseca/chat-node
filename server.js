dd81c0c5f7589b85b5bf03ef44087385f4c37bb3
Andriw Fonseca <andriw.fonseca@totvs.com.br>
2025-10-20 15:39:05 -0300

    feat: establishing connection

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
+});
+
+io.on('connection', (socket) => {
+  console.log('Um usuário se conectou: ', socket.id);
+
+  
 });
